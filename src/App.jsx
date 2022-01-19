418090e7a5c360876503679402cac96eb600f2ba
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-01-19 23:59:09 +0100

    feat: layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from './Header.jsx';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
       <div className="container-view">Some activities should be here</div>
+      <Footer />
     </div>
   );
 };
