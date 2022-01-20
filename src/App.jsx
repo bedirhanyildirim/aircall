1cbb1d62ad3ca003a53f4c7abb45245b1a2fb03c
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-01-20 03:18:09 +0100

    feat: router created

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
-import store from "./redux/store";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
+import CallList from "./components/CallList.jsx";
+import CallDetails from "./components/CallDetails.jsx";
 
-const App = () => {
+function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Header/>
-      <div className="container-view">Some activities should be here</div>
-      <Footer />
+      <div className="container-view">
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<CallList/>}/>
+                  <Route path="/call" element={<CallDetails/>}/>
+              </Routes>
+          </BrowserRouter>
+      </div>
+      <Footer/>
     </div>
   );
-};
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
-  document.getElementById('app'));
+}
 
 export default App;
